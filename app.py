3d414f1706e2debbb94fbcb49b37bb09351a7df9
Scott McAllister <mr.scott.mcallister@gmail.com>
2017-03-02 22:54:29 -0500

    get titles for every horror movie since 1960

--- app.py
+++ app.py
@@ -3,6 +3,22 @@ from bs4 import BeautifulSoup
 
 list_of_horror_films = 'https://en.wikipedia.org/wiki/Lists_of_horror_films'
 
+
+def innerHTML(element):
+    return element.decode_contents(formatter="html")
+
+
+def read_wiki_list_table(url):
+    print('\n' + url + '\n')
+    with urllib.request.urlopen(url) as wiki_list_response:
+        wiki_list_html = wiki_list_response.read()
+        wiki_list_soup = BeautifulSoup(wiki_list_html, "html.parser")
+        table = wiki_list_soup.find_all('table')[1]
+        title_links = table.select('th a')
+        for title_link in title_links:
+            print(innerHTML(title_link))
+
+
 with urllib.request.urlopen(list_of_horror_films) as response:
     html = response.read()
     soup = BeautifulSoup(html, "html.parser")
@@ -10,4 +26,5 @@ with urllib.request.urlopen(list_of_horror_films) as response:
     for ul_tag in ul_tags:
         a_tags = ul_tag.find_all('a')
         for a_tag in a_tags:
-            print(a_tag['href'])
+            list_page = 'https://en.wikipedia.org' + a_tag['href']
+            read_wiki_list_table(list_page)
